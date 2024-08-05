b418ee4135c4dd94cacda5fe60829225689e0f33
Prabej Hussain <contact.prabej@gmail.com>
2024-08-05 20:01:24 +0545

    Add hostel and room view in user account

--- src/pages/account/Display/Hostel.tsx
+++ src/pages/account/Display/Hostel.tsx
@@ -1,16 +1,31 @@
 import HostelCard from "@/components/ui/HostelCard";
-
+import H from "@/constant/types/Hostels";
+import url from "@/constant/url";
+import axios from "axios";
+import { useState, useEffect } from "react";
 const Hostel: React.FC = () => {
+  const [hostels, setHostels] = useState<H[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const fetchHostels = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`${url}get-hostels`);
+      setHostels(response.data);
+    } catch (e) {
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchHostels();
+  }, []);
+  if (loading) return <div>Loading...</div>;
   return (
     <>
       <div className="max-h-80 overflow-y-auto w-[1000px] grid grid-cols-2 gap-6">
-        <HostelCard />
-        <HostelCard />
-        <HostelCard />
-        <HostelCard />
-        <HostelCard />
-        <HostelCard />
-        <HostelCard />
+        {hostels.map((hostel) => {
+          return <HostelCard />;
+        })}
       </div>
     </>
   );
--- src/pages/account/Display/Rooms.tsx
+++ src/pages/account/Display/Rooms.tsx
@@ -1,22 +1,40 @@
 import RoomCard from "@/components/ui/RoomsCard";
 import { useEffect } from "react";
-
+import R from "@/constant/types/rooms";
+import { useState } from "react";
+import axios from "axios";
+import url from "@/constant/url";
 const Rooms: React.FC = () => {
+  const [rooms, setRooms] = useState<R[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const fetchRoom = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`${url}rooms`);
+      setRooms(response.data);
+      setLoading(false);
+    } catch (e) {
+    } finally {
+    }
+  };
+  useEffect(() => {
+    fetchRoom();
+  }, []);
   useEffect(() => {});
+  if (loading) return <div>Loading...</div>;
   return (
     <>
       <div className="max-h-80 overflow-y-auto w-[1000px] grid grid-cols-2">
-        <RoomCard title="Title" description="1 Room" id="1" thumbnail="1" />
-        <RoomCard title="Title" description="1 Room" id="1" thumbnail="1" />
-        <RoomCard title="Title" description="1 Room" id="1" thumbnail="1" />
-        <RoomCard title="Title" description="1 Room" id="1" thumbnail="1" />
-        <RoomCard
-          title="Title"
-          description="1 Room"
-          id="1"
-          thumbnail="1"
-        />{" "}
-        <RoomCard title="Title" description="1 Room" id="1" thumbnail="1" />
+        {rooms.map((room, index) => {
+          return (
+            <RoomCard
+              title={room.address}
+              description={`Rooms :${room.noOfRooms}`}
+              id={room._id}
+              thumbnail={room.img[0]}
+            />
+          );
+        })}
       </div>
     </>
   );
